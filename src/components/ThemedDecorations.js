5a3b227caaccfa1a4554a567f37e0552a29a9291
Matt Hough <matt@hough.tv>
2018-02-07 09:29:52 +1000

    Done.

--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,1 +1,34 @@
-// Code ThemedDecoration Component Here
+import React from 'react';
+
+class ThemedDecorations extends React.Component {
+  render() {
+    const childrenWithExtraProp = React.Children.map(this.props.children, child => {
+      return React.cloneElement(child, {
+        className: this.props.theme
+      });
+    });
+
+    return (
+      <div>{childrenWithExtraProp}</div>
+    );
+  }
+}
+
+export default ThemedDecorations;
+
+
+// render() {
+//     const currentPlayingTitle = 'Mad Max: Fury Road';
+//     const childrenWithExtraProp = React.Children.map(this.props.children, child => {
+//       return React.cloneElement(child, {
+//         isPlaying: child.props.title === currentPlayingTitle
+//       });
+//     });
+ 
+//     return (
+//       <div className="movie-browser">
+//         {childrenWithExtraProp}
+//       </div>      
+//     );
+//   }
+// }
